2d6c46f229fdb73683ac4533420174f9eb0c40dc
Yudi Yamane <yudiyamane@gmail.com>
2023-04-22 17:59:15 -0300

    feat: request básico

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,15 +2,42 @@ import fs from 'fs';
 import type { PageServerLoad } from './$types';
 
 type SomeType = {
-	post: string;
 	dirContent: string[];
+	users: User[];
 };
 
-export const load: PageServerLoad<SomeType> = () => {
+type User = {
+	id: number;
+	login: string;
+	avatar_url: string;
+	followers_url: string;
+	following_url: string;
+};
+
+export const load: PageServerLoad<SomeType> = async () => {
 	const dirContent = fs.readdirSync('.');
 
+	const token = '...';
+	const org = 'projeto-de-algoritmos';
+	const response = await fetch(
+		`https://api.github.com/orgs/${org}/members?` +
+			new URLSearchParams({
+				filter: 'all',
+				per_page: '100',
+				page: '3',
+			}),
+		{
+			headers: {
+				Accept: ' application/vnd.github+json',
+				'X-GitHub-Api-Version': '2022-11-28',
+				Authorization: `Bearer ${token}`,
+			},
+		}
+	);
+	const users: User[] = await response.json();
+
 	return {
-		post: 'hey',
 		dirContent,
+		users,
 	};
 };
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,15 @@
 	export let data: PageData;
 </script>
 
+<svelte:head>
+	<title>Rede de Usuários GitHub</title>
+</svelte:head>
+
 <h1>Welcome to SvelteKit</h1>
+
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<span> {data.post} </span>
+<pre> {data.users.length} </pre>
+<pre>{JSON.stringify(data.users, null, 2)}</pre>
 
 <p>{data.dirContent}</p>
