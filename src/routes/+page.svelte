5ddffebdfa3987ec8a1bb9485106a977a7af5a8d
Yudi Yamane <yudiyamane@gmail.com>
2023-04-24 22:52:47 -0300

    feat: adiciona pesquisa por login WIP
    Co-authored-by: Vitor Alves <vitor.alves07750@outlook.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,20 +4,47 @@
 	export let data: PageData;
 
 	const logins = data.users.map((u) => u.login);
+
 	const sg = new SymbolGraph(logins, data.connections);
+	let filter = '';
+
+	let filteredLogins: string[] = [];
 
 	const g = sg.graph();
+
+	const filterLogins = (e: Event & { currentTarget: EventTarget & HTMLSelectElement }) => {
+		console.log('filtering...');
+
+		filteredLogins = [];
+		for (let i = 0; i < logins.length; i++) {
+			if (logins[i].indexOf(e.currentTarget.value) >= 0) {
+				filteredLogins.push(logins[i]);
+			}
+		}
+
+		console.log(filteredLogins);
+
+		console.log('filtering... done');
+	};
 </script>
 
 <svelte:head>
 	<title>Rede de Usuários GitHub</title>
 </svelte:head>
 
-<pre>{g.toString()}</pre>
+<select>
+	{#if filter.length === 0}
+		{#each logins as login}
+			<option value={login}>{login}</option>
+		{/each}
+	{:else}
+		{#each filteredLogins as login}
+			<option value={login}>{login}</option>
+		{/each}
+	{/if}
+</select>
 
-<p>Quantidade de usuários: {data.users.length}</p>
-
-<p>Quantidade de nós: {data.connections.length}</p>
+<input type="text" on:keyup={filterLogins} bind:value={filter} />
 
 <div style="display: grid; grid-template-columns: 1fr 4fr;">
 	{#each data.connections as node}
