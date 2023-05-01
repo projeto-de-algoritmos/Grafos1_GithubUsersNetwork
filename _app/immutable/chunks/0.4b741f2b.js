dd3812a69a1200bc73c84facec6b493b191feb86
Yudi Yamane <yudiyamane@gmail.com>
2023-05-01 20:33:34 -0300

    Updates

---
+++ _app/immutable/chunks/0.4b741f2b.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/layout.svelte.49b060e4.js";export{t as component};
---
+++ _app/immutable/chunks/2.42715b1a.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.67ec07f1.js";export{t as component};
